cfffd500d7f4fc3b751c52cbbdb7585a7c53ddc6
Matthew Nakamura <matthew.t.nakamura@gmail.com>
2025-06-18 10:07:17 -1000

    Apply black formatting to noxfile.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,61 +5,55 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+
+
+sys.path.insert(0, os.path.abspath("../src"))
 
 # -- Project information -----------------------------------------------------
-project = 'Fishing Line Material Properties Analysis'
-copyright = '2025, Nanosystems Lab'
-author = 'Nanosystems Lab'
-release = '0.0.1'
+project = "Fishing Line Material Properties Analysis"
+copyright = "2025, Nanosystems Lab"
+author = "Nanosystems Lab"
+release = "0.0.1"
 
 # Custom title for the HTML pages
-html_title = f'{project}<br/>v{release} documentation'
+html_title = f"{project}<br/>v{release} documentation"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-    'myst_parser',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "myst_parser",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output ------------------------------------------------
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
 
 # PyData Sphinx Theme configuration
 html_theme_options = {
     # Header links
     "github_url": "https://github.com/nanosystemslab/Fishing_Line_Material_Properties_Analysis",
     "use_edit_page_button": True,
-    
     # Navigation and TOC
     "show_toc_level": 2,
     "navbar_align": "left",
     "navbar_center": ["navbar-nav"],
     "navbar_end": ["navbar-icon-links"],
-    
     # Site navigation
     "external_links": [
-        {
-            "name": "Nanosystems Lab",
-            "url": "https://github.com/nanosystemslab"
-        }
+        {"name": "Nanosystems Lab", "url": "https://github.com/nanosystemslab"}
     ],
-    
     # Footer
     "footer_start": ["copyright"],
     "footer_end": ["sphinx-version"],
-    
     # Search
     "search_bar_text": "Search the docs...",
-    
     # Page elements - simplified to avoid template errors
     "show_prev_next": True,
 }
@@ -79,11 +73,11 @@ html_css_files = [
 
 # -- Options for autodoc ----------------------------------------------------
 autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'show-inheritance': True,
-    'special-members': '__init__',
-    'exclude-members': '__weakref__'
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+    "special-members": "__init__",
+    "exclude-members": "__weakref__",
 }
 
 # Generate autosummary stub files
@@ -95,10 +89,10 @@ autodoc_mock_imports = []
 
 # -- Options for intersphinx extension --------------------------------------
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'pandas': ('https://pandas.pydata.org/docs/', None),
-    'matplotlib': ('https://matplotlib.org/stable/', None),
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
 }
 
 # -- Options for MyST parser --------------------------------------------
